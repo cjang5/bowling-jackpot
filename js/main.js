61e4f0bb5c8767ae275a3aee2424109bc5ecec4a
Chris Jang <chrismangggg@gmail.com>
2015-11-01 23:40:36 -0600

    Added really basic functionality

--- js/main.js
+++ js/main.js
@@ -1,46 +1,75 @@
 // Create instance of the Bowling-Api-Client
 var client = new BowlingApiClient('http://bowling-api.nextcapital.com/api');
 
-// create a user on button click
-$("#createUser").click(function() {
-    console.log("Creating user...");
+// current user's username (email) and password
+var username = '';
+var password = '';
+// is the user logged in? Defaulted to 'false'
+var logged_in = false;
+
+// update the status of user
+var updateStatus = function() {
+    if (logged_in) {
+        $('#userStatus').text('Logged in as: ' + username);
+    } else {
+        $('#userStatus').text('Not logged in.');
+    }
+}
+
+// Login button
+$('#login_button').click(function() {
+    // get the text from the two input forms
+    var userText = $('#username').val();
+    var passText = $('#password').val();
     
-    client.createUser({
-        email: 'test43@example.com',
-        password: 'password',
+    console.log('Logging in...');
+    
+    // attempt login via Bowling Api
+    client.loginUser({
+        email: userText,
+        password: passText,
         success: function(user) {
+            alert('Successfully logged in: ' + userText);
+            console.log(JSON.stringify(user, null, 4));
+            
+            // flip the 'logged_in' flag and update status
+            logged_in = true;
+            username = userText; // update username
+            password = passText; // update password
+            updateStatus();
+        },
+        error: function(xhr) { 
+            alert('Login failed'); // TODO: Determine whether it was incorrect email or password using 
+            console.log(JSON.parse(xhr.responseText));
+        }
+    });
+});
+
+// Register button
+$('#register_button').click(function() {
+    // get the text from the two input forms
+    var userText = $('#username').val();
+    var passText = $('#password').val();
+    
+    console.log('Registering...');
+    
+    // attempt to register with given credentials
+    client.createUser({
+        email: userText,
+        password: passText,
+        success: function(user) {
+            alert('Successfully registered as: ' + userText);
             console.log(user);
+            
+            // flip 'logged_in' flag and update status
+            logged_in = true;
+            username = userText; // update username
+            password = passText; // update password
+            updateStatus();
         },
         error: function(xhr) {
-            console.log("ERROR: " + JSON.parse(xhr.responseText));
-        }
-    });
-});
-
-$('#getBowlers').click(function() {
-    console.log('Getting leagues...');
-    
-    client.getBowlers({
-        success: function(bowlers) {
-            console.log(bowlers);
-        },
-        error: function(xhr) {
-            console.log('ERROR: ' + JSON.parse(xhr.responseText));
-        }
-    });
-});
-
-$('#getBowler390').click(function() {
-    console.log('Getting bowler with id 390...');
-    
-    client.getBowler({
-        bowlerId: 731,
-        success: function(bowler) {
-            $('#paragraph').text(JSON.stringify(bowler, null, 4));
-            console.log(JSON.stringify(bowler, null, 4));
-        },
-        error: function(xhr) {
-            console.log('ERROR: ' + JSON.parse(xhr.responseText));
+            alert('Account with provided email already exists!');
+            console.log(JSON.parse(xhr.responseText));
         }
     });
 });
