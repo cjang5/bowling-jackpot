83f27abc2761c552e2ad6facc3b333ad1a809649
cjang5 <chrismangggg@gmail.com>
2015-11-06 00:13:02 -0600

    Fixed overflow of sidebar buttons being hoverable

--- js/client.js
+++ js/client.js
@@ -14,6 +14,42 @@ $('.sidebar a.open').click(function(e) {
     $('.right').toggleClass('open-nav2');
     $('.sidebar .s-text').toggleClass('appear');
     
+    // Home button
+    if ($('#Home').text() == '') 
+        $('#Home').text('Home');
+    else 
+        setTimeout(function() { $('#Home').text(''); }, 500);
+    
+    // Feed button
+    if ($('#Feed').text() == '')
+        $('#Feed').text('Feed');
+    else
+        setTimeout(function() { $('#Feed').text(''); }, 500);
+    
+    // Bowlers button
+    if ($('#Bowlers').text() == '')
+        $('#Bowlers').text('Bowlers');
+    else
+        setTimeout(function() { $('#Bowlers').text(''); }, 500);
+    
+    // Leagues button
+    if ($('#Leagues').text() == '')
+        $('#Leagues').text('Leagues');
+    else
+        setTimeout(function() { $('#Leagues').text(''); }, 500);
+    
+    // Jackpots button
+    if ($('#Jackpots').text() == '')
+        $('#Jackpots').text('Jackpots');
+    else
+        setTimeout(function() { $('#Jackpots').text(''); }, 500);
+    
+    // Collapse button
+    if ($('#Collapse').text() == '')
+        $('#Collapse').text('Collapse');
+    else
+        setTimeout(function() { $('#Collapse').text(''); }, 500);
+    
     // make everything look pretty when the sidebar is toggled
     $('.sidebar .sText').toggleClass('toggle');
     $('.sidebar .s-icon').toggleClass('toggle2');
