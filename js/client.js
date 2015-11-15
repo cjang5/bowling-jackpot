b5e784d08c35c3fd6b6ebdbdc7179c982143b7f0
cjang5 <chrismangggg@gmail.com>
2015-11-15 15:32:23 -0600

    Added client.js, added some basic view handling

--- js/client.js
+++ js/client.js
@@ -1,12 +1,41 @@
-$('#clickme').click(function() {
-    alert(sessionStorage.getItem('logged_in')); 
-    console.log('Logged in as: ' + sessionStorage.getItem('username'));
+/**
+ * Main view handling code
+ * On load, the current view will be the News Feed
+ * 0 = News Feed
+ * 1 = Bowlers
+ * 2 = Leagues
+ * 3 = Jackpots
+ */
+var currPage = 0;
+/*
+$('.Bowlers-button').click(function() {
+    currPage = 1;
+    // hide the current active view and display this one
+    $('.active').removeClass('active');
+    $('.bowlers-view').addClass('active');
+});*/
+
+// Show the news div first
+$(".main-view div").each(function(){
+    $(this).hide();
+    if($(this).attr('id') == "news") {
+        $(this).show();
+    }
 });
 
-$('#home').click(function() {
-    window.open('index.html', '_self', false);
+// Based on which sidebar button is clicked, show appropriate view
+$('.sidebar .view-button').on( "click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr('data-related'); 
+    $(".main-view div").each(function(){
+        $(this).hide();
+        if($(this).attr('id') == id) {
+            $(this).show();
+        }
+    });
 });
 
+// Sidebar code
 $('.sidebar a.open').click(function(e) {
     e.preventDefault();
     
